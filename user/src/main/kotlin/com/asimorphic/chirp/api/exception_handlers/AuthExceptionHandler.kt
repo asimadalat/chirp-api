695dffb88cd96071d9d98259a9593752b8fd1e35
asim <90917263+asimadalat@users.noreply.github.com>
2025-11-01 17:58:55 +0000

    Added JWT service, token generation/verification/parsing methods & exception handler for invalid tokens

--- user/src/main/kotlin/com/asimorphic/chirp/api/exception_handlers/AuthExceptionHandler.kt
+++ user/src/main/kotlin/com/asimorphic/chirp/api/exception_handlers/AuthExceptionHandler.kt
@@ -1,5 +1,6 @@
 package com.asimorphic.chirp.api.exception_handlers
 
+import com.asimorphic.chirp.domain.exception.InvalidTokenException
 import com.asimorphic.chirp.domain.exception.UserAlreadyExistsException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -14,6 +15,10 @@ class AuthExceptionHandler {
     @ExceptionHandler(UserAlreadyExistsException::class)
     @ResponseStatus(HttpStatus.CONFLICT)
     fun onUserAlreadyExists(ex: UserAlreadyExistsException) = mapOf("code" to "USER_EXISTS", "message" to ex.message)
+
+    @ExceptionHandler(InvalidTokenException::class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    fun onInvalidToken(ex: InvalidTokenException) = mapOf("code" to "INVALID_TOKEN", "message" to ex.message)
 
     @ExceptionHandler(MethodArgumentNotValidException::class)
     fun onValidationException(ex: MethodArgumentNotValidException): ResponseEntity<Map<String, Any>> {
---
+++ user/src/main/kotlin/com/asimorphic/chirp/domain/exception/InvalidTokenException.kt
@@ -0,0 +1,3 @@
+package com.asimorphic.chirp.domain.exception
+
+class InvalidTokenException(override val message: String?): RuntimeException(message ?: "Invalid token")
